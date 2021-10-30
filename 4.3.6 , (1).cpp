7c5cfedc2ede21c72d6f6a3f413c1822b5de5145
Жеребятин Илья ТКИ 112 <91667591+ilya31231@users.noreply.github.com>
2021-10-30 15:22:00 +0300

    Update 4.3.6 , (1).cpp

--- 4.3.6 , (1).cpp
+++ 4.3.6 , (1).cpp
@@ -16,121 +16,164 @@ int main(int argc, char *argv[])
     {
     
     
-    int x, y;
-    cin >> x >> y;
+    int x, y, q, w;
+	cout << "Введите размеры массива" << endl;
+    cin >> x >> y; //Вводится размеры массива
     int a[x][y];
+	int v[x][y];
     
     srand(time(NULL));
-    
+    cout << "Вот ваш массив" << endl;
     for (int i = 0; i < x; i++)
         for (int j = 0; j < y; j++)
-            a[i][j] = rand() ; 
-            
+			{
+				a[i][j] = rand() ; //заполяем массив рандомными элементами
+				v[i][j] = a[i][j];
+			}
+		
+
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
-            cout << a[i][j] << "  ";
+            cout << a[i][j] << "  "; //выводим массив
         cout << endl;
+		
     }
-    
-     for (int i = 0; i < x; i++)
+
+	cout << endl;
+	
+    cout << "После выполнения пункта 1 массив стал выглядить вот так:" << endl;
+    for (int i = 0; i < x; i++)
     {
         int max = 0;
         for (int j = 0; j < y; j++)
             if (a[i][j] > a[i][max])
-                max = j;
-        a[i][max] *= 0;
+                max = j; //находим максимальный элемент в строке
+        a[i][max] *= 0; //заменяем элемент на 0
     }
-    cout << endl << endl;
-    
-    
-    if (k == 2)
+
+		
+	for (int i = 0; i < x; i++)
     {
+        for (int j = 0; j < y; j++)
+            cout << a[i][j] << "  "; //выводим массив
+        cout << endl;
+		
+    }
+
+		
+    cout << endl << endl;
+
+		
+    if (k == 2) //проверяем, нужен ли пункт 2 
+    {
+	cout << "После выполнения 2-го пункта, массив выглядит вот так:" << endl;	
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             int f = j;
             int ff = 0;
-            if(a[i][0] % 3 == 0 && f == 0 && a[f][0] != 0)
+            if(v[i][0] % 3 == 0 && f == 0 && v[f][0] != 0)
             {
-                for (int j = 0; j < y; j++)
-                    cout << "0" << " ";
+				
+                for (int mm = 0; mm < y; mm++)
+                {
+                    cout << "0" << " ";// Вставляем перед всеми строками, первый элемент которых делится на 3, строку
+					
+                }
             }
-            cout << a[i][j] << "  ";
+            if(v[i][0] % 3 == 0 && f == 0 && v[f][0] != 0)
+            {
+                cout << endl;
+            }
+			
+            cout << v[i][j] << "  ";
             ff += 1;
         }
         cout << endl;
     }
     }
-    else
-    {
-        for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            cout << a[i][j] << "  ";
-        }
-        cout << endl;
-    }
-    }
+    
         
     }
     
     else
     {
-        int x, y;
-    cin >> x >> y;
+    int x, y, q, w;
+	cout << "Введите размеры массива" << endl;
+    cin >> x >> y; //Вводится размеры массива
     int a[x][y];
+	int v[x][y];
     
     srand(time(NULL));
-    
+    cout << "Заполните массив вводос с клавиатуры" << endl;
     for (int i = 0; i < x; i++)
         for (int j = 0; j < y; j++)
-            cin >> a[i][j] ; 
-            
+			{
+				
+				cin >> a[i][j] ; //вводим массив с клавиатуры
+				v[i][j] = a[i][j];
+			}
+	cout << "Вот ваш массив" << endl;
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
-            cout << a[i][j] << "  ";
+            cout << a[i][j] << "  "; //выводим массив
         cout << endl;
+		
     }
-    
-     for (int i = 0; i < x; i++)
+
+	cout << endl;
+	
+    cout << "После выполнения пункта 1 массив стал выглядить вот так:" << endl;
+    for (int i = 0; i < x; i++)
     {
         int max = 0;
         for (int j = 0; j < y; j++)
             if (a[i][j] > a[i][max])
-                max = j;
-        a[i][max] *= 0;
+                max = j; //находим максимальный элемент в строке
+        a[i][max] *= 0; //заменяем элемент на 0
     }
-    cout << endl << endl;
-    if (k == 2)
+
+		
+	for (int i = 0; i < x; i++)
     {
+        for (int j = 0; j < y; j++)
+            cout << a[i][j] << "  "; //выводим массив
+        cout << endl;
+		
+    }
+
+		
+    cout << endl << endl;
+
+		
+    if (k == 2) //проверяем, нужен ли пункт 2 
+    {
+	cout << "После выполнения 2-го пункта, массив выглядит вот так:" << endl;	
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             int f = j;
             int ff = 0;
-            if(a[i][0] % 3 == 0 && f == 0 && a[f][0] != 0)
+            if(v[i][0] % 3 == 0 && f == 0 && v[f][0] != 0)
             {
-                for (int j = 0; j < y; j++)
-                    cout << "0" << " ";
+				
+                for (int mm = 0; mm < y; mm++)
+                {
+                    cout << "0" << " ";// Вставляем перед всеми строками, первый элемент которых делится на 3, строку
+					
+                }
             }
-            cout << a[i][j] << "  ";
+            if(v[i][0] % 3 == 0 && f == 0 && v[f][0] != 0)
+            {
+                cout << endl;
+            }
+			
+            cout << v[i][j] << "  ";
             ff += 1;
-        }
-        cout << endl;
-    }
-    }
-    else
-    {
-        for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            cout << a[i][j] << "  ";
         }
         cout << endl;
     }
